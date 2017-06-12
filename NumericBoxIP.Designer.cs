572bec1b6d58c5873b9f36f86d6fb03269194cff
MarekG73 <galuszkamarek73@gmail.com>
2017-06-12 23:58:31 +0200

    Pierwszy wrzut

---
+++ NumericBoxIP.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace NewControls
+{
+    partial class NumericBoxIP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ipBoxFieldA = new System.Windows.Forms.TextBox();
+            this.ipBoxFieldB = new System.Windows.Forms.TextBox();
+            this.ipBoxFieldC = new System.Windows.Forms.TextBox();
+            this.ipBoxFieldD = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 7;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.label3, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ipBoxFieldA, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ipBoxFieldB, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ipBoxFieldC, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ipBoxFieldD, 6, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(195, 26);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(145, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = ".";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(92, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = ".";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // ipBoxFieldA
+            // 
+            this.ipBoxFieldA.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ipBoxFieldA.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.ipBoxFieldA.Location = new System.Drawing.Point(3, 3);
+            this.ipBoxFieldA.MaxLength = 0;
+            this.ipBoxFieldA.Name = "ipBoxFieldA";
+            this.ipBoxFieldA.Size = new System.Drawing.Size(30, 20);
+            this.ipBoxFieldA.TabIndex = 0;
+            this.ipBoxFieldA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ipBoxFieldA.WordWrap = false;
+            this.ipBoxFieldA.TextChanged += new System.EventHandler(this.ipBoxField_TextChanged);
+            this.ipBoxFieldA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ipBoxField_KeyPress);
+            // 
+            // ipBoxFieldB
+            // 
+            this.ipBoxFieldB.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ipBoxFieldB.Location = new System.Drawing.Point(56, 3);
+            this.ipBoxFieldB.MaxLength = 0;
+            this.ipBoxFieldB.Name = "ipBoxFieldB";
+            this.ipBoxFieldB.Size = new System.Drawing.Size(30, 20);
+            this.ipBoxFieldB.TabIndex = 1;
+            this.ipBoxFieldB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ipBoxFieldB.TextChanged += new System.EventHandler(this.ipBoxField_TextChanged);
+            this.ipBoxFieldB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ipBoxField_KeyPress);
+            // 
+            // ipBoxFieldC
+            // 
+            this.ipBoxFieldC.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ipBoxFieldC.Location = new System.Drawing.Point(109, 3);
+            this.ipBoxFieldC.MaxLength = 0;
+            this.ipBoxFieldC.Name = "ipBoxFieldC";
+            this.ipBoxFieldC.Size = new System.Drawing.Size(30, 20);
+            this.ipBoxFieldC.TabIndex = 2;
+            this.ipBoxFieldC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ipBoxFieldC.TextChanged += new System.EventHandler(this.ipBoxField_TextChanged);
+            this.ipBoxFieldC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ipBoxField_KeyPress);
+            // 
+            // ipBoxFieldD
+            // 
+            this.ipBoxFieldD.Location = new System.Drawing.Point(162, 3);
+            this.ipBoxFieldD.MaxLength = 0;
+            this.ipBoxFieldD.Name = "ipBoxFieldD";
+            this.ipBoxFieldD.Size = new System.Drawing.Size(30, 20);
+            this.ipBoxFieldD.TabIndex = 3;
+            this.ipBoxFieldD.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ipBoxFieldD.TextChanged += new System.EventHandler(this.ipBoxField_TextChanged);
+            this.ipBoxFieldD.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ipBoxField_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(39, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = ".";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // NumericBoxIP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "NumericBoxIP";
+            this.Size = new System.Drawing.Size(195, 26);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox ipBoxFieldA;
+        private System.Windows.Forms.TextBox ipBoxFieldB;
+        private System.Windows.Forms.TextBox ipBoxFieldD;
+        private System.Windows.Forms.TextBox ipBoxFieldC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ NumericBoxIP.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace NewControls
+{
+    /// <summary>
+    /// Kontrolka do wprowadzania numeru IP.
+    /// Zawiera sprawdzenie poprawności:
+    /// -ogranicza wartość do 255,
+    /// -uniemożliwia wprowadzanie innych znaków niż cyfry
+    /// </summary>
+    public partial class NumericBoxIP: UserControl
+    {
+        #region Zmienne
+
+        private int[] _ipNumbers;
+        private TextBox[] _TextFields;
+
+        #endregion
+
+        #region Konstruktory
+        /// <summary>
+        /// Podstawowy konstruktor - ustawia adres 0.0.0.0
+        /// </summary>
+        public NumericBoxIP()
+        {
+            InitializeComponent();
+
+            _ipNumbers = new int[4];
+            _TextFields = new TextBox[4] { ipBoxFieldA, ipBoxFieldB, ipBoxFieldC, ipBoxFieldD };
+
+            for (int ipOct = 0; ipOct < 4; ipOct++)
+            {
+                _TextFields[ipOct].Text = "0";
+                _ipNumbers[ipOct] = 0;
+            }
+        }
+        /// <summary>
+        /// Konstruktor ustawiający adres na 127.0.0.0 
+        /// </summary>
+        /// <param name="p_isLocalhost">Wartość <i>true</i> ustawia <i>localhost</i></param>
+        public NumericBoxIP(bool p_isLocalhost)
+        {
+            InitializeComponent();
+
+            _ipNumbers = new int[4];
+            _TextFields = new TextBox[4] { ipBoxFieldA, ipBoxFieldB, ipBoxFieldC, ipBoxFieldD };
+
+            if (p_isLocalhost == true)
+            {
+                _TextFields[0].Text = "127";
+                _ipNumbers[0] = 127;
+                for (int ipOct = 1; ipOct < 4; ipOct++)
+                {
+                    _TextFields[ipOct].Text = "0";
+                    _ipNumbers[ipOct] = 0;
+                }
+            }
+            else
+            {
+                for (int ipOct = 0; ipOct < 4; ipOct++)
+                {
+                    _TextFields[ipOct].Text = "0";
+                    _ipNumbers[ipOct] = 0;
+                }
+            }
+        }
+        /// <summary>
+        /// Konstruktor umożliwiający nadanie początkowych wartości
+        /// </summary>
+        /// <param name="p_a">Pierwszy oktet - max. 255</param>
+        /// <param name="p_b">Drugi  oktet - max. 255</param>
+        /// <param name="p_c">Trzeci  oktet - max. 255</param>
+        /// <param name="p_d">Czwarty  oktet - max. 255</param>
+        public NumericBoxIP(int p_a, int p_b, int p_c, int p_d)
+        {
+            InitializeComponent();
+
+            _ipNumbers = new int[4] { p_a > 255 ? 255:p_a, p_b > 255 ? 255 : p_b, p_c > 255 ? 255 : p_c, p_d > 255 ? 255 : p_d };
+            _TextFields = new TextBox[4] { ipBoxFieldA, ipBoxFieldB, ipBoxFieldC, ipBoxFieldD };
+
+            for (int ipOct = 0; ipOct < 4; ipOct++)
+            {
+                _TextFields[ipOct].Text = _ipNumbers[ipOct].ToString();
+            } 
+        }
+        #endregion
+
+        #region Funkcje zwracające
+        /// <summary>
+        /// Funkcja zwraca tablicę z numerem IP
+        /// </summary>
+        /// <returns>int[4]</returns>
+        public int[] GetNumericIP()
+        {
+            _ipNumbers[0] = int.Parse(ipBoxFieldA.Text);
+            _ipNumbers[1] = int.Parse(ipBoxFieldB.Text);
+            _ipNumbers[2] = int.Parse(ipBoxFieldC.Text);
+            _ipNumbers[3] = int.Parse(ipBoxFieldD.Text);
+
+            return _ipNumbers;
+        }
+        /// <summary>
+        /// Funkcja zwraca łańcuch tekstowy z numerem IP
+        /// </summary>
+        /// <returns>string</returns>
+        public string GetTextIP()
+        {
+            string ipText = "";
+            for (int ipOct = 0; ipOct < 4; ipOct++)
+            {
+                ipText += _TextFields[ipOct].Text + ".";
+            }
+            return ipText.Remove(ipText.Length - 1);
+        }
+        /// <summary>
+        /// Funkcja zwraca numer IP jako osobne oktety w formie tekstowej
+        /// </summary>
+        /// <returns>IEnumerable<string></returns>
+        public IEnumerable<string> GetFragmentedTextIP()
+        {
+            for (int ipOct = 0; ipOct < 4; ipOct++)
+            {
+                yield return _TextFields[ipOct].Text;
+            }
+            yield break;
+        }
+        #endregion
+
+        #region Funkcje wewnętrzne
+        //Sprawdzanie wpisywanych znaków
+        private char CheckCharValue(string p_value)
+        {
+            int result = 0;
+
+            if (int.TryParse(p_value, out result))
+            {
+                return char.Parse(p_value);
+            }
+            return ' ';
+        }
+        //Sprawdzanie wpisanej liczby (max. 255)
+        private string CheckTotalValue(string p_totalValue)
+        {
+            int result = 0;
+            if (int.TryParse(p_totalValue, out result) && result < 256)
+            {
+                return p_totalValue;
+            }
+            return "";
+        }
+        #endregion
+
+        #region Zdarzenia
+        private void ipBoxField_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Sprawdzenie naciśniętego klawisza - możliwe tylko cyfry i sterujące
+            TextBox usedBox = sender as TextBox;
+            if (usedBox.Text.Length < 4 && e.KeyChar != 8)
+            {
+                e.KeyChar = CheckCharValue(e.KeyChar.ToString());
+            }
+            else 
+            {
+                if (usedBox.Text.Length > 0)
+                {
+                    usedBox.Text.Remove(usedBox.Text.Length - 1);
+                }       
+            }
+        }
+        private void ipBoxField_TextChanged(object sender, EventArgs e)
+        {
+            TextBox usedBox = sender as TextBox;
+            usedBox.Text = CheckTotalValue(usedBox.Text);
+        }
+        #endregion
+    }
+}
